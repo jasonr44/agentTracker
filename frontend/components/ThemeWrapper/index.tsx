32ad1f97f2b5bc8941466e3ae938489ce4a3bc97
Jason Ransom <jasonlawrenceransom@gmail.com>
2020-11-13 15:39:38 -0500

    Added theme.

--- frontend/components/ThemeWrapper/index.tsx
+++ frontend/components/ThemeWrapper/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { ThemeProvider, ThemeProviderProps } from '@material-ui/core/styles';
+import { ThemeProvider, Theme } from '@material-ui/core/styles';
 import AgentTheme from 'styles/theme';
 import CssBaseline from '@material-ui/core/CssBaseline';
+
+interface PartialThemeProviderProps {
+  children: React.ReactNode;
+  theme?: Partial<Theme> | ((outerTheme: Theme) => Theme);
+}
 
 const ThemeWrapper = ({
   children,
   theme = AgentTheme,
-}: ThemeProviderProps): JSX.Element => {
+}: PartialThemeProviderProps): JSX.Element => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
